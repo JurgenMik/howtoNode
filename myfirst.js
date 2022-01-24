b1c97a374e22ffbed98ffb10be1ed2f270414bd1
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-01-24 18:17:59 +0200

    Node File-system: Read Files

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,11 @@
-
 var http = require('http');
-var dt = require('./myfirstmodule')
-http.createServer(function (req, res) {
+var fs = require('fs');
+
+// server object loomine
+http.createServer(function(req,res) {
+    fs.readFile('demofile1.html', function(err, data){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime())
-    res.end();
-}).listen(8080);
+    res.write(data);
+    return res.end();
+});
+}).listen(8080); // Response is listened at port 8080
