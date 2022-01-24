4766b48ff66130ddb7b6f37066d85fd16b5df4ca
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-01-24 17:40:21 +0200

    Node modules - exp function DATE

--- myfirst.js
+++ myfirst.js
@@ -1,5 +1,8 @@
+
 var http = require('http');
+var dt = require('./myfirstmodule')
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World! My name is ');
+    res.write("The date and time are currently: " + dt.myDateTime())
+    res.end();
 }).listen(8080);
