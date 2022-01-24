48f57e07989c9c3cd2e6b9535b121facfb2a5b5e
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-01-24 18:29:34 +0200

    Node File-system: Rename a file with fs.rename

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 
-fs.unlink('mynewfile2.txt', function(err){
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt',  function(err){
     if (err) throw err;
-    console.log('deleted');
-
+    console.log('FILE RENAMED');
 });
