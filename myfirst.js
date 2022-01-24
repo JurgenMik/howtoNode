4b136673813f442972a2c91d518e00e54b045571
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-01-24 18:24:34 +0200

    Node File-system: Create a file with fs.open

--- myfirst.js
+++ myfirst.js
@@ -1,11 +1,7 @@
-var http = require('http');
 var fs = require('fs');
 
-// server object loomine
-http.createServer(function(req,res) {
-    fs.readFile('demofile1.html', function(err, data){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+fs.open('mynewfile2.txt', 'w', function(err,file){
+    if (err) throw err;
+    console.log('Saved!');
+
 });
-}).listen(8080); // Response is listened at port 8080
